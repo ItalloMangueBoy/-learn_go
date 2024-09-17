e9e7b98fa52840929b7b6d061ee5eb9b0b31a170
Emika <itallomkspop@gmail.com>
2024-09-16 22:54:51 -0300

    learn how to define variables in go

--- 2 - variables/main.go
+++ 2 - variables/main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("hello")
+	var v1 int = 1
+
+	v2 := 2
+
+	var (
+		v3 int = 3
+		v4 int = 4
+	)
+
+	v5, v6 := 5, 6
+
+	fmt.Println(v1, v2, v3, v4, v5, v6)
 }
